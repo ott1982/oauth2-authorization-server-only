5a4232b810158849cc4630c4ddef391b51bbf450
Oriol <oriol.tristany@eurecat.org>
2019-09-13 08:50:04 +0200

    wqinitial commit

---
+++ src/main/java/com/orioltristany/oauth2authorizationserveronly/Oauth2AuthorizationServerOnlyApplication.java
@@ -0,0 +1,12 @@
+package com.orioltristany.oauth2authorizationserveronly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthorizationServerOnlyApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(Oauth2AuthorizationServerOnlyApplication.class, args);
+	}
+}
